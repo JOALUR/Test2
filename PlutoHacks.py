fd307ed8d38c08293d0a402efc54bf02eee95a25
Jose Uribe <116236557+JOALUR@users.noreply.github.com>
2022-10-22 00:08:44 -0400

    Test from hackathon

--- PlutoHacks.py
+++ PlutoHacks.py
@@ -1,2 +1,12 @@
 #comment
 print("hello world")
+Variable  = input("Do you have a significant other")
+if Variable == "Yes":
+    print("I'm happy for you, do you enjoy their company.")
+elif Variable == "No":
+    print("Did you have one before?")
+Variable = input("Do you have grandchildre?")
+if Variable == "Yes":
+    print("How much!? I hear having grandchildren is amazing.")
+elif Variable == "No":
+    print("I hope you get the chance to get some one day.")
